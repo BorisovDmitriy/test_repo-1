49e18333bd594b06306d0815b2bab05a9f56b95f
Dmitriy B <you@example.com>
2023-05-23 21:20:38 +0400

    test-repo: vetka-2 create url and bag

--- test_rep/test_rep/urls.py
+++ test_rep/test_rep/urls.py
@@ -19,4 +19,8 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ne verno/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 ]
